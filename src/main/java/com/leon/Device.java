8adb36c83597059220870aa5bd6c866c346e2fc2
liyang <liyang11@asiainfo.com>
2016-11-01 13:48:54 +0800

    update

---
+++ src/main/java/com/leon/Device.java
@@ -0,0 +1,34 @@
+package com.leon;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement(name="device")
+public class Device {
+	private String deviceIp;
+	private int deviceStatus;
+	public Device() {
+	}
+	public Device(String deviceIp){
+		super();
+		this.deviceIp=deviceIp;
+	}
+	
+	public String getIp(){
+		return deviceIp;
+	}
+	
+	public void setIp(String deviceIp){
+		this.deviceIp=deviceIp;
+	}
+	
+	@XmlAttribute
+	public int getStatus(){
+		return deviceStatus;
+	}
+	
+	public void setStatus(int deviceStatus){
+		this.deviceStatus=deviceStatus;
+	}
+}
